0e40111c740414249ae18f6c14c587f8309bad7a
Lin Han <me@linhan.email>
2022-03-12 23:03:46 -0500

    dep

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -17,7 +17,7 @@ setup(
     name="pycocotoolse",
     packages=["pycocotoolse"],
     package_dir={"pycocotoolse": "pycocotoolse"},
-    install_requires=["setuptools>=18.0", "cython>=0.27.3", "matplotlib>=2.1.0"],
-    version="2.0.3",
+    install_requires=["numpy", "setuptools>=18.0", "cython>=0.27.3", "matplotlib>=2.1.0"],
+    version="2.0.4",
     ext_modules=ext_modules,
 )
